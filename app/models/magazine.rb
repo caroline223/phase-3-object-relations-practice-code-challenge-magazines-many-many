a1522bee62598d29640ba1dd50df810405f9223f
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-11-25 22:25:26 -0500

    Complete deliverables

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,17 @@
 class Magazine < ActiveRecord::Base
+  has_many :subscriptions
+  has_many :readers, through: :subscriptions
+
+
+  def email_list
+    email_arr = self.readers.map{|reader| reader.email}
+    email_arr.join(";")
+  end
   
+  def self.most_popular
+    most_popular = Magazine.all.max_by do |mag_inst|
+        mag_inst.subscriptions.count 
+    end
+  end
+
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,21 @@
 class Reader < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :magazines, through: :subscriptions
+
+  def subscribe(magazine, price)
+    Subscription.create(magazine_id: magazine.id, reader_id: self.id, price: price)
+  end
+
+  def total_subscription_price
+    self.subscriptions.sum(:price)
+  end
+
+  def cancel_subscription(magazine)
+    sub = Subscription.find_by(magazine_id: magazine.id , reader_id: self.id)
+    if sub
+        sub.destroy
+    end
+  end
+
+
 end
